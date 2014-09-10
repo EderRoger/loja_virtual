eae2efddf77dd4d15136b33944b5dd20326ea344
Eder Roger Souza <eder.roger.souza@gmail.com>
2014-09-09 21:32:42 -0300

    add some features

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,5 +1,17 @@
+# encoding: utf-8
+
 class Livro
-	def initialize(autor, isbn = "1", numero_de_paginas)
-		puts "Autor: #{autor}, isbn: #{isbn}, Págs: #{numero_de_paginas}"
+
+    attr_accessor :preco
+
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+		@preco = preco
+	end
+
+	def to_s
+	  "Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
 	end
 end
